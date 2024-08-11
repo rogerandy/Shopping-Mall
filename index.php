da1e159141854bce4e22bfa3e355876ab8ab28f9
roger <yayv332211@gmail.com>
2024-08-11 16:26:25 +0800

    index revised

--- index.php
+++ index.php
@@ -9,7 +9,54 @@
 </head>
 
 <body>
+    <!-- 開場動畫 -->
+    <div class="animated">
 
+    </div>
+
+    <header>
+        <nav>
+            <ul>
+                <div>
+                    <h2>2024 Latest Website</h2>
+                    <!-- <img src="" alt="">
+                    <i><a href=""></a></i> -->
+                </div>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Category</li>
+                <li>Content</li>
+                <li>Story</li>
+                <li>Cart</li>
+            </ul>
+            <div id="photo">
+                <!-- 放照片 -->
+                <div></div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <div id="category">
+                <div id="photo"></div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <!-- 個人簡介 -->
+        <div>
+            <div><i></i></div>
+            <div><i></i></div>
+            <div><i></i></div>
+            <div><i></i></div>
+        </div>
+    </footer>
+
+    <!-- 移動是icon -->
+    <div id="icon"></div>
 </body>
+<script src="./app.js"></script>
 
 </html>
