7dd7c72520fefaba990b7d4092b67ca6c48e6f4f
roger <yayv332211@gmail.com>
2024-08-12 16:28:05 +0800

    add front page

--- index.php
+++ index.php
@@ -5,37 +5,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Mall</title>
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- cdnjs -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
     <link rel="icon" href="./images/cart-shopping-solid.svg">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
     <!-- 開場動畫 -->
+
     <div class="animated">
 
     </div>
 
+    <!-- 固定圖片 -->
+    <div class="fix-img">
+
+    </div>
     <header>
         <nav>
-            <ul>
-                <div>
-                    <h2>2024 Latest Website</h2>
-                    <!-- <img src="" alt="">
-                    <i><a href=""></a></i> -->
-                </div>
-                <li>Home</li>
-                <li>Product</li>
-                <li>Category</li>
-                <li>Content</li>
-                <li>Story</li>
-                <li>Cart</li>
-            </ul>
-            <div id="photo">
-                <!-- 放照片 -->
-                <div></div>
+            <div>
+
+                <i><a href=""></a></i>
             </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="./Login.php">Login</a></li>
+                <li><a href="./Product">Product</a></li>
+                <li><a href="./Content.php">Content</a></li>
+                <li><a href="./Story.php">Story</a></li>
+                <li><a href="./Cart.php">Cart</a></li>
+            </ul>
         </nav>
+        <div id="photo">
+            <!-- 放照片 -->
+            <div id="display">
+                <img src="" alt="">
+            </div>
+        </div>
     </header>
 
+    <!-- 第二層放照片 -->
     <main>
         <section>
             <div id="category">
